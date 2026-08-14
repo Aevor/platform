e1983a3f22b064600de0eaad4f13a3e67b85ea92
Sanjeev Kumar <sanjeevkumar_s@srmap.edu.in>
2026-08-14 21:26:15 +0530

    feat: add upsert-by-github-id and persist access tokens

--- services/api/internal/users/dto.go
+++ services/api/internal/users/dto.go
@@ -1,17 +1,5 @@
 package users
 
-type CreateUserRequest struct {
-	GithubID int64 `json:"github_id"`
-
-	Username string `json:"username"`
-
-	DisplayName string `json:"display_name"`
-
-	Email string `json:"email"`
-
-	AvatarURL string `json:"avatar_url"`
-}
-
 type UserResponse struct {
 	ID string `json:"id"`
 
